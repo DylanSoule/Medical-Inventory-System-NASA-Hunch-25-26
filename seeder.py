a187362329ce22542f8326b381dd3a92d10695f2
DylanSoule <dylan.soule@icloud.com>
2026-02-15 18:33:48 -0700

    database schema

--- seeder.py
+++ seeder.py
@@ -4,7 +4,8 @@ import mysql.connector
 liteconn = sqlite3.connect('Database/inventory.db')
 
 myconn = mysql.connector.connect(
-    host="localhost",
+    host="127.0.0.1",
+    port=3306,
     user="root",
     password="1234",
     database="inventory_system"
@@ -14,11 +15,11 @@ lite_c = liteconn.cursor()
 
 my_c = myconn.cursor()
 
-lite_c.execute("SELECT barcode,estimated_amount FROM drugs_in_inventory;")
+lite_c.execute("SELECT barcode,estimated_amount FROM drugs;")
 drugs = lite_c.fetchall()
 
 for entry in drugs:
-    my_c.execute("INSERT INTO in_inventory (barcode, amount) VALUES (%s, %s)", (entry[0],entry[1]))
+    my_c.execute("INSERT INTO medications (barcode, name, amount_in_unit, type, dosage) VALUES (%s, %s,%s,%s,%s)", (entry[0],entry[1], entry[2],entry[4],entry[5]+entry[6]))
 
 liteconn.close()
 
